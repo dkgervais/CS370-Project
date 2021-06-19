5988d23921126c201429ff41e3b9832951cbf43d
Danielle G <dgervais@colostate.edu>
2021-06-19 16:33:09 +0000

    Update air-quality.py

--- code/air-quality.py
+++ code/air-quality.py
@@ -5,9 +5,9 @@ import time
 
 def calculate_air_quality():
 	try:
-    	sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+    		sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 	except (RuntimeError, IOError):
-    	sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+    		sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 
 	# Default oversampling settings
 
@@ -33,75 +33,75 @@ def calculate_air_quality():
 	burn_in_data = []
 
 	try:
-    	# Collect gas resistance burn-in values, then use the average
-    	# of the last 50 values to set the upper limit for calculating
-    	# gas_baseline.
-    	print('Collecting gas resistance burn-in data for 50 secs\n')
-    	while curr_time - start_time < burn_in_time:
-        	curr_time = time.time()
-        	if sensor.get_sensor_data() and sensor.data.heat_stable:
-            	gas = sensor.data.gas_resistance
-            	burn_in_data.append(gas)
-            	print('Gas: {0} Ohms'.format(gas))
-            	time.sleep(1)
+    		# Collect gas resistance burn-in values, then use the average
+    		# of the last 50 values to set the upper limit for calculating
+    		# gas_baseline.
+   	 	print('Collecting gas resistance burn-in data for 50 secs\n')
+    		while curr_time - start_time < burn_in_time:
+        		curr_time = time.time()
+        		if sensor.get_sensor_data() and sensor.data.heat_stable:
+ 	        	   	gas = sensor.data.gas_resistance
+        	    		burn_in_data.append(gas)
+            			print('Gas: {0} Ohms'.format(gas))
+            			time.sleep(1)
 		
-    	gas_baseline = sum(burn_in_data[-50:]) / 50.0
+    		gas_baseline = sum(burn_in_data[-50:]) / 50.0
+	
+   	 	# Set the humidity baseline to 40%, an optimal outdoor humidity.
+		# The actual optimal outdoor humidity is between 30-50%, but it
+      		# has been averaged to 40% for this equation
+    		hum_baseline = 40.0
 
-    	# Set the humidity baseline to 40%, an optimal outdoor humidity.
-      # The actual optimal outdoor humidity is between 30-50%, but it
-      # has been averaged to 40% for this equation
-    	hum_baseline = 40.0
+    		# This sets the balance between humidity and gas reading in the
+    		# calculation of air_quality_score (25:75, humidity:gas)
+    		hum_weighting = 0.25
 
-    	# This sets the balance between humidity and gas reading in the
-    	# calculation of air_quality_score (25:75, humidity:gas)
-    	hum_weighting = 0.25
-
-    	print('Gas baseline: {0} Ohms, humidity baseline: {1:.2f} %RH\n'.format(
-        	gas_baseline,
-        	hum_baseline))
+    		print('Gas baseline: {0} Ohms, humidity baseline: {1:.2f} %RH\n'.format(
+       		 	gas_baseline,
+        		hum_baseline))
 
 		# Reset the timers to record air quality 		
 		start_time = time.time()
 		curr_time = time.time()
 
-    	while curr_time - start_time < burn_in_time:
+    		while curr_time - start_time < burn_in_time:
 			curr_time = time.time()
-        	if sensor.get_sensor_data() and sensor.data.heat_stable:
-        	    gas = sensor.data.gas_resistance
-        	    gas_offset = gas_baseline - gas
+        		if sensor.get_sensor_data() and sensor.data.heat_stable:
+        	    		gas = sensor.data.gas_resistance
+        	    		gas_offset = gas_baseline - gas
 	
-        	    hum = sensor.data.humidity
-        	    hum_offset = hum - hum_baseline
+        	    	hum = sensor.data.humidity
+        	    	hum_offset = hum - hum_baseline
 
-        	    # Calculate hum_score as the distance from the hum_baseline.
-        	    if hum_offset > 0:
-        	        hum_score = (100 - hum_baseline - hum_offset)
-        	        hum_score /= (100 - hum_baseline)
-        	        hum_score *= (hum_weighting * 100)
+        	    	# Calculate hum_score as the distance from the hum_baseline.
+        	    	if hum_offset > 0:
+        	        	hum_score = (100 - hum_baseline - hum_offset)
+        	        	hum_score /= (100 - hum_baseline)
+        	        	hum_score *= (hum_weighting * 100)
 
-        	    else:
-        	        hum_score = (hum_baseline + hum_offset)
-        	        hum_score /= hum_baseline
-        	        hum_score *= (hum_weighting * 100)
+        	    	else:
+        	        	hum_score = (hum_baseline + hum_offset)
+        	        	hum_score /= hum_baseline
+        	        	hum_score *= (hum_weighting * 100)
 
-        	    # Calculate gas_score as the distance from the gas_baseline.
-        	    if gas_offset > 0:
-        	        gas_score = (gas / gas_baseline)
-        	        gas_score *= (100 - (hum_weighting * 100))
+        	    	# Calculate gas_score as the distance from the gas_baseline.
+        	    	if gas_offset > 0:
+        	        	gas_score = (gas / gas_baseline)
+        	        	gas_score *= (100 - (hum_weighting * 100))
+		
+        		else:
+        	        	gas_score = 100 - (hum_weighting * 100)
 	
-        	    else:
-        	        gas_score = 100 - (hum_weighting * 100)
-	
-        	    # Calculate air_quality_score.
-        	    air_quality_score = hum_score + gas_score
-				total_air_quality += air_quality_score				
+        	    	# Calculate air_quality_score.
+        	    	air_quality_score = hum_score + gas_score
+			total_air_quality += air_quality_score				
 				
-        	    print('Gas: {0:.2f} Ohms,humidity: {1:.2f} %RH,air quality: {2:.2f}'.format(
-        	        gas,
-        	        hum,
-        	        air_quality_score))
+        	    	print('Gas: {0:.2f} Ohms,humidity: {1:.2f} %RH,air quality: {2:.2f}'.format(
+        	        	gas,
+        	        	hum,
+        	        	air_quality_score))
 	
-        	    time.sleep(1)
+        	    	time.sleep(1)
 	
 	except KeyboardInterrupt:
     	pass
